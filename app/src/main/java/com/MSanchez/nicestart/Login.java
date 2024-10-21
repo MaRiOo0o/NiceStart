25d1e51c6a2d4e26175983dcaf3bf2584b93d23d
MaRiOo0o <masansedi@gmail.com>
2024-10-21 12:46:29 +0200

    Sustitucion de Main por Login, creacion de la version land se sing up y correccion de gradientes

--- app/src/main/java/com/MSanchez/nicestart/Login.java
+++ app/src/main/java/com/MSanchez/nicestart/Login.java
@@ -10,14 +10,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class Login extends AppCompatActivity {
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -25,12 +25,12 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-  /*  public void openMain(View v){
-        Intent intent = new Intent(MainActivity.this, MainActivity.class);
-    }   */
+    /*  public void openMain(View v){
+          Intent intent = new Intent(MainActivity.this, MainActivity.class);
+      }   */
     public void openSignup(View v){
-     Intent intent = new Intent(MainActivity.this, Signup.class);
-     startActivity(intent);
+        Intent intent = new Intent(Login.this, Signup.class);
+        startActivity(intent);
 
     }
 }
