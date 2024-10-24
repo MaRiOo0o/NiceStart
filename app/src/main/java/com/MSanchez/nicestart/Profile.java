2dca81afa2163dc433a2e503ecea3a2f6071ffa6
MaRiOo0o <masansedi@gmail.com>
2024-10-24 11:41:13 +0200

    Refactorizacion del main a profile y cambio en la forma de carga de las imagenes

--- app/src/main/java/com/MSanchez/nicestart/Profile.java
+++ app/src/main/java/com/MSanchez/nicestart/Profile.java
@@ -1,8 +1,6 @@
 package com.MSanchez.nicestart;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
@@ -14,16 +12,13 @@ import androidx.core.view.WindowInsetsCompat;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
 
-public class Login extends AppCompatActivity {
-
+public class Profile extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
-
+        setContentView(R.layout.activity_profile);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -32,22 +27,12 @@ public class Login extends AppCompatActivity {
         });
         ImageView mGirl=findViewById(R.id.girl);
         Glide.with(this)
-//                .load("https://images.unsplash.com/photo-1489424731084-a5d8b219a5bb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80")
-                .load(R.drawable.girl)
-                .transition(DrawableTransitionOptions.withCrossFade(2000))
-                .centerCrop()
-//                .circleCrop()
+              .load("https://images.unsplash.com/photo-1489424731084-a5d8b219a5bb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80")
+//
+                .transition(DrawableTransitionOptions.withCrossFade(1000))
+//                .centerCrop()
+                .circleCrop()
 //                .placeholder(new ColorDrawable(this.getResources().getColor(R.color.teal_200)))
                 .into(mGirl);
-    }
-    public void openProfile(View v) {
-        Intent intent = new Intent(Login.this, Profile.class);
-        startActivity(intent);
-
-    }
-    public void openSignup(View v){
-        Intent intent = new Intent(Login.this, Signup.class);
-        startActivity(intent);
-
     }
 }
--- app/src/main/java/com/MSanchez/nicestart/Signup.java
+++ app/src/main/java/com/MSanchez/nicestart/Signup.java
@@ -3,12 +3,16 @@ package com.MSanchez.nicestart;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
 
 public class Signup extends AppCompatActivity {
 
@@ -22,9 +26,19 @@ public class Signup extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        ImageView mBoy=findViewById(R.id.boy);
+        Glide.with(this)
+//                .load("https://images.unsplash.com/photo-1489424731084-a5d8b219a5bb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80")
+                .load(R.drawable.hipman)
+                .transition(DrawableTransitionOptions.withCrossFade(2000))
+                .centerCrop()
+//                .circleCrop()
+//                .placeholder(new ColorDrawable(this.getResources().getColor(R.color.teal_200)))
+                .into(mBoy);
+
     }
-    public void openMain(View v) {
-        Intent intent = new Intent(Signup.this, Main.class);
+    public void openProfile(View v) {
+        Intent intent = new Intent(Signup.this, Profile.class);
         startActivity(intent);
 
     }
